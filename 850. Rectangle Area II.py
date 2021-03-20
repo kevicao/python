abcb442e96cf44242851708f28a75b2bde851b5e
Jack <kevicao@users.noreply.github.com>
2021-03-20 00:36:35 -0700

    Update 850. Rectangle Area II.py

--- 850. Rectangle Area II.py
+++ 850. Rectangle Area II.py
@@ -1,6 +1,8 @@
 850. Rectangle Area II
 
 
+https://leetcode.com/problems/rectangle-area-ii/solution/
+
 
 # Suppose instead of rectangles = [[0,0,2,2],[1,0,2,3],[1,0,3,1]], we had [[0,0,200,200],[100,0,200,300],[100,0,300,100]]. The answer would just be 100 times bigger.
 
@@ -42,4 +44,56 @@ class Solution(object):
             for y, val in enumerate(row):
                 if val:
                     ans += (imapx[x+1] - imapx[x]) * (imapy[y+1] - imapy[y])
+        return ans % (10**9 + 7)
+
+
+
+Approach #2: Line Sweep
+Intuition
+
+Imagine we pass a horizontal line from bottom to top over the shape. We have some active intervals on this horizontal line, which gets updated twice for each rectangle. In total, there are 2 * N2∗N events, and we can update our (up to NN) active horizontal intervals for each update.
+
+Algorithm
+
+For a rectangle like rec = [1,0,3,1], the first update is to add [1, 3] to the active set at y = 0, and the second update is to remove [1, 3] at y = 1. Note that adding and removing respects multiplicity - if we also added [0, 2] at y = 0, then removing [1, 3] at y = 1 will still leave us with [0, 2] active.
+
+This gives us a plan: create these two events for each rectangle, then process all the events in sorted order of y. The issue now is deciding how to process the events add(x1, x2) and remove(x1, x2) such that we are able to query() the total horizontal length of our active intervals.
+
+We can use the fact that our remove(...) operation will always be on an interval that was previously added. Let's store all the (x1, x2) intervals in sorted order. Then, we can query() in linear time using a technique similar to a classic LeetCode problem, Merge Intervals.
+
+class Solution(object):
+    def rectangleArea(self, rectangles):
+        # Populate events
+        OPEN, CLOSE = 0, 1
+        events = []
+        for x1, y1, x2, y2 in rectangles:
+            events.append((y1, OPEN, x1, x2))
+            events.append((y2, CLOSE, x1, x2))
+        events.sort()
+
+        def query():
+            ans = 0
+            cur = -1
+            for x1, x2 in active:
+                cur = max(cur, x1)
+                ans += max(0, x2 - cur)
+                cur = max(cur, x2)
+            return ans
+
+        active = []
+        cur_y = events[0][0]
+        ans = 0
+        for y, typ, x1, x2 in events:
+            # For all vertical ground covered, update answer
+            ans += query() * (y - cur_y)
+
+            # Update active intervals
+            if typ is OPEN:
+                active.append((x1, x2))
+                active.sort()
+            else:    
+                active.remove((x1, x2))
+
+            cur_y = y
+
         return ans % (10**9 + 7)
