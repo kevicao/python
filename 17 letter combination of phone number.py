c96893e7090ff04fc83d37367c1173e8acc9390e
Jack <kevicao@users.noreply.github.com>
2021-03-30 05:44:46 -0700

    Create 17 letter combination of phone number.py

---
+++ 17 letter combination of phone number.py
@@ -0,0 +1,21 @@
+[mw_shl_code=python,true]class Solution:
+    def letterCombinations(self, digits):        
+        if not digits or len(digits) == 0:
+            return []
+        
+        dic = {"2":"abc", "3":"def",
+               "4":"ghi", "5":"jkl",
+               "6":"mno", "7":"pqrs",
+               "8":"tuv", "9":"wxyz"}
+        
+        res = []
+        self.dfs(digits, dic, 0, "", res)-baidu 1point3acres
+        return res
+   
+    def dfs(self, digits, dic, index, path, res):
+        if len(path) == len(digits):
+            res.append(path)
+            return
+        
+        for j in dic[digits[index]]:
+            self.dfs(digits, dic, index + 1, path + j, res)
