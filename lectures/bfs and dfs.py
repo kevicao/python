2f4735e2710312ac324544e3ee61dee4d41519de
Jack <kevicao@users.noreply.github.com>
2022-09-05 20:35:26 -0700

    Update bfs and dfs.py

--- lectures/bfs and dfs.py
+++ lectures/bfs and dfs.py
@@ -41,6 +41,34 @@ class Solution(object):
 s = Solution()
 s.levelOrder(n3)
 
+# bfs
+def printLevelOrder(root):
+    # Base Case
+    if root is None:
+        return
+ 
+    # Create an empty queue
+    # for level order traversal
+    queue = []
+ 
+    # Enqueue Root and initialize height
+    queue.append(root)
+ 
+    while(len(queue) > 0):
+ 
+        # Print front of queue and
+        # remove it from queue
+        print(queue[0].data)
+        node = queue.pop(0)
+ 
+        # Enqueue left child
+        if node.left is not None:
+            queue.append(node.left)
+ 
+        # Enqueue right child
+        if node.right is not None:
+            queue.append(node.right)
+
 # dfs
 def printInorder(root):
     if root:
